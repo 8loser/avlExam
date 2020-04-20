ca418ed5ae751028660b1d38eb7ca149d6d7ea0b
8loser@gmail.com <8loser@gmail.com>
2020-04-20 14:28:45 +0800

    刪除demo檔案

--- functions/routes/demoRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/demoController");
-
-// 設定路徑執行動作
-router.get("/", controller.get);
-// 帶參數
-router.get("/:param", controller.get);
-
-module.exports = router;
